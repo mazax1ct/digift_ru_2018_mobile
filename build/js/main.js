e0bdfed852108a594025eceae035fd282e50b8c9
mazax1ct <vova@giveandget.ru>
2018-08-27 00:26:49 +0300

    main page without main-menu

---
+++ build/js/main.js
@@ -0,0 +1,90 @@
+//активация кнопки "наверх"
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 30) {
+    $('.header').addClass('scrolled');
+    $('.js-to-top').addClass('is-active');
+  } else {
+    $('.header').removeClass('scrolled');
+    $('.js-to-top').removeClass('is-active');
+  }
+});
+
+//скролл наверх при нажитии кнопки "наверх"
+$('.js-to-top').click(function () {
+  $('body, html').animate({
+      scrollTop: 0
+  }, 300);
+  return false;
+});
+
+$(document).ready(function() {
+  //слайдер карточек
+  if ($('.js-cards-slider').length) {
+    $('.js-cards-slider').slick({
+      adaptiveHeight: true,
+      arrows: false,
+      infinite: false,
+      edgeFriction: 0,
+      dots: true,
+      variableWidth: true
+    });
+  }
+
+  //форма
+  $('.js-input').each(function( index ) {
+    if ($(this).val() != 0) {
+      $(this).addClass('changed');
+    } else {
+      $(this).removeClass('changed');
+    }
+
+    $(this).blur(function() {
+      if ($(this).val() != 0) {
+        $(this).addClass('changed');
+      } else {
+        $(this).removeClass('changed');
+      }
+    });
+  });
+
+  //открытие/закрытие главного меню
+  $(".js-menu-opener").click(function() {
+    $(this).toggleClass("is-active");
+    $("body").toggleClass("overflow");
+    $(".main-menu-block").toggleClass("is-open");
+    return false;
+  });
+
+  //замена ссылок в контактах
+  if($('.js-change-link').length){
+    $('.js-change-link').each(function( index ) {
+      var replaceText = $(this).attr('data-link');
+      $(this).html(replaceText);
+    });
+  }
+
+  //карточки - аккордеоны
+  $(".accordion__title-block").click(function() {
+    var item = $(this).parent('.accordion');
+    var list = item.parent('.accordions');
+    if(item.hasClass('is-open')){
+      item.removeClass('is-open');
+    } else {
+      list.find('.accordion').removeClass("is-open");
+      item.addClass('is-open');
+    }
+    return false;
+  });
+
+  //заголовки - аккордеоны
+  $(".accordion-title__title").click(function() {
+    $(this).parent('.accordion-title').toggleClass('is-open');
+    return false;
+  });
+
+  //меню в футере - аккордеоны
+  $(".footer-menu__title").click(function() {
+    $(this).parent('.footer-menu').toggleClass('is-open');
+    return false;
+  });
+});
